c48f0c510ce9b0ec7510571f33192b125516b40a
Semo <tpah20@gmail.com>
2022-06-24 17:22:56 +0900

    update

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel
 
 import uvicorn
 from fastapi import FastAPI
+import requests, asyncio, logging
+
 
 
 #버킷과 파일 이름은 여기서 결정된다. 다른 곳에서는 이 값을 받아와 사용
@@ -19,14 +21,9 @@ app = FastAPI()
 
 FLSe = ServerStatus()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
 @app.get("/FLSe/info")
 def read_status():
+    global FLSe
     print('Server_status: ')
     print(FLSe)
     print()
@@ -47,7 +44,38 @@ def update_ready(FLSeReady: bool):
         FLSe.GL_Model_V += 1
     return {"Server_Status": FLSe}
 
+@app.on_event("startup")
+def startup():
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
+
+    loop.create_task(fl_server_check())
+
+def async_dec(awaitable_func):
+    async def keeping_state():
+        while True:
+            try:
+                logging.debug(str(awaitable_func.__name__) + '함수 시작')
+                # print(awaitable_func.__name__, '함수 시작')
+                await awaitable_func()
+                logging.debug(str(awaitable_func.__name__) + '_함수 종료')
+            except Exception as e:
+                # logging.info('[E]' , awaitable_func.__name__, e)
+                logging.error('[E]' + str(awaitable_func.__name__) + str(e))
+            await asyncio.sleep(1)
+    return keeping_state
+
+@async_dec
+async def fl_server_check():
+    res = requests.get('http://10.152.183.249:8080/FL_ST')
+    if res.status_code != 200:
+        FLSe.FLSeReady=False
+    else:
+        FLSe.FLSeReady=True
+    return FLSe
 
 
-if __name__ == "__main__":
+
+if __name__ == "__main__":    
+
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
