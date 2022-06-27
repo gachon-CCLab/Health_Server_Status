cfe3d30e230b48db39b39e9c2be4462de28fafa8
Semo <tpah20@gmail.com>
2022-06-27 13:13:46 +0900

    2

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import uvicorn
 from fastapi import FastAPI
 import requests, asyncio, logging
 
-
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)8.8s] %(message)s",
+                    handlers=[logging.StreamHandler()])
+logger = logging.getLogger(__name__)
 
 #버킷과 파일 이름은 여기서 결정된다. 다른 곳에서는 이 값을 받아와 사용
 #
@@ -44,35 +46,35 @@ def update_ready(FLSeReady: bool):
         FLSe.GL_Model_V += 1
     return {"Server_Status": FLSe}
 
-# @app.on_event("startup")
-# def startup():
-#     loop = asyncio.get_event_loop()
-#     loop.set_debug(True)
+@app.on_event("startup")
+def startup():
+    loop = asyncio.get_event_loop()
+    loop.set_debug(True)
 
-#     loop.create_task(fl_server_check())
+    loop.create_task(fl_server_check())
 
-# def async_dec(awaitable_func):
-#     async def keeping_state():
-#         while True:
-#             try:
-#                 logging.debug(str(awaitable_func.__name__) + '함수 시작')
-#                 # print(awaitable_func.__name__, '함수 시작')
-#                 await awaitable_func()
-#                 logging.debug(str(awaitable_func.__name__) + '_함수 종료')
-#             except Exception as e:
-#                 # logging.info('[E]' , awaitable_func.__name__, e)
-#                 logging.error('[E]' + str(awaitable_func.__name__) + str(e))
-#             await asyncio.sleep(1)
-#     return keeping_state
+def async_dec(awaitable_func):
+    async def keeping_state():
+        while True:
+            try:
+                logging.debug(str(awaitable_func.__name__) + '함수 시작')
+                # print(awaitable_func.__name__, '함수 시작')
+                await awaitable_func()
+                logging.debug(str(awaitable_func.__name__) + '_함수 종료')
+            except Exception as e:
+                # logging.info('[E]' , awaitable_func.__name__, e)
+                logging.error('[E]' + str(awaitable_func.__name__) + str(e))
+            await asyncio.sleep(1)
+    return keeping_state
 
-# @async_dec
-# async def fl_server_check():
-#     res = requests.get('http://10.152.183.249:8080/FL_ST')
-#     if res.status_code != 200:
-#         FLSe.FLSeReady=False
-#     else:
-#         FLSe.FLSeReady=True
-#     return FLSe
+@async_dec
+async def fl_server_check():
+    res = requests.get('http://localhost:8001/FL_ST')
+    if res.status_code != 200:
+        FLSe.FLSeReady=False
+    else:
+        FLSe.FLSeReady=True
+    return FLSe
 
 
 
